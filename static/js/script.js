6d458f64ac276ef7add789e7a8983ebfe80e78d9
asifdanishyar <asifdanishyar@yahoo.com>
2020-08-27 19:24:37 +0000

    style css is updated

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,18 @@
-
 $(document).ready(function() {
-    $(".carousel").carousel();
-    setInterval(function() {
-        $(".carousel").carousel("next");
-    }, 3000);
     // code initializer for collapsible
     $(".collapsible").collapsible();
     // code initializer for select
     $("select").material_select();
     // code initializer for side navbar
     $(".button-collapse").sideNav();
-    $('select').formSelect();
-    
-    
+    // code initializer for carousel(image slider)
+    $(".carousel").carousel();
+    setInterval(function() {
+        $(".carousel").carousel("next");
+    }, 2000);
     // code initializer for modal
     $(".modal").modal();
+    $("select").formselect();
     // code initializer for flash message
     $(".alert.success").hide().delay(1000).fadeIn(200).delay(4000).fadeOut(200);
     // code initializer for character counter in input fields
@@ -29,15 +27,3 @@ $("select[required]").css({
     width: 0,
     position: "absolute",
 });
-$(".datepicker").pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: "Today",
-    clear: "Clear",
-    close: "Ok",
-    closeOnSelect: false, // Close upon selecting a date,
-});
-
-// Added underline code to make date picker field required
-// source stack overflow
-$(".datepicker").prop("readonly", false);
